33873235ece7cc2b8f595e3321bcd95e6036ab96
Cuvilliez <fiona.cuvilliez@etu.univ-cotedazur.fr>
2026-05-09 09:32:34 +0200

    ajout dijkstra

--- min_cost_flow.py
+++ min_cost_flow.py
@@ -75,29 +75,29 @@ def min_cost_flow_bellman_ford(graph: List[List[int]],cost: List[List[int]],s: i
 
 
 #2ème approche avec l'algorithme de Dijkstra
-def dijkstra(graph, cost, s, potential):
+def dijkstra(graph: List[List[int]],cost: List[List[int]],s: int, potential: List[int]) -> Tuple[List[float, List[int]]]:
+    # étape 1 initialisation
     n = len(graph)
-    dist = [float("inf")] * n
+    dist = [float("inf")] * n #pour chaque simmet on initialise à +inf
     parent = [-1] * n
+    dist[s] = 0 #sommet de départ à 0
+    priority_queue = [(0, s)] #file de priorité
 
-    dist[s] = 0
-    pq = [(0, s)]
-
-    while pq:
-        d, u = heappop(pq)
-
-        if d > dist[u]:
+    while priority_queue:
+        current_dist, u = heappop(priority_queue) #on retire le noeud
+        #on ignore si plus la bonne distance
+        if current_dist > dist[u]:
             continue
-
-        for v in range(n):
-            if graph[u][v] > 0:
-                # coût réduit
+        for v in range(n):  #on regarde les voisins
+            if graph[u][v] > 0: #arête dans le graphe
+                # on normalise le cout
                 new_cost = cost[u][v] + potential[u] - potential[v]
-
-                if dist[u] + new_cost < dist[v]:
-                    dist[v] = dist[u] + new_cost
+                new_dist = dist[u] + new_cost
+                #relaxation
+                if new_dist < dist[v]:
+                    dist[v] = new_dist
                     parent[v] = u
-                    heappush(pq, (dist[v], v))
+                    heappush(priority_queue, (dist[v], v))
 
     return dist, parent
 
@@ -105,43 +105,43 @@ def dijkstra(graph, cost, s, potential):
 
 def min_cost_flow_dijkstra(graph: List[List[int]],cost: List[List[int]],s: int, t:int) -> Tuple[List[List[int]], List[List[int]], int, int]:
     n = len(graph)
-    flow = 0
+    max_flow = 0
     total_cost = 0
-    # potentiels
+    # potentiels pour normaliser
     potential = [0] * n
+     # matrice de flot
+    flow = [[0]*n for _ in range(n)]
+
     while True:
         dist, parent = dijkstra(graph, cost, s, potential)
-
         if parent[t] == -1:
             break
-
         # mise à jour des potentiels
         for i in range(n):
             if dist[i] < float("inf"):
                 potential[i] += dist[i]
-
         # trouver flow minimum
         path_flow = float("inf")
         v = t
-
         while v != s:
             u = parent[v]
             path_flow = min(path_flow, graph[u][v])
             v = u
-
-        flow += path_flow
+            
+        max_flow += path_flow
         total_cost += path_flow * potential[t]
-
         # mise à jour du graphe
         v = t
         while v != s:
             u = parent[v]
-
+            #capacité résiduel
             graph[u][v] -= path_flow
             graph[v][u] += path_flow
-
+            #arête inverse ducoût oppposé
             cost[v][u] = -cost[u][v]
-
+            #mise à jour du flot
+            flow[u][v] += path_flow
+            flow[v][u] -= path_flow
             v = u
 
-    return flow, total_cost
+    return flow, graph, max_flow, total_cost
